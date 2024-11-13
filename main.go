6e7d095c177229e53adf03d7ea982358f753a1f5
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-13 11:19:49 -0500

    version and test api

--- main.go
+++ main.go
@@ -6,10 +6,18 @@ import (
 
 func main() {
 	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
 	app.Get("/test", handleTest)
+	apiv1.Get("/user", handleUser)
+
 	app.Listen(":5000")
 }
 
 func handleTest(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msg": "I am working..."})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"user": "John Doe"})
 }
