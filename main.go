7ff9629e5db9ecc9035e58185ef8cdbdf4fbd1a7
nodojo <52790569+nodojo@users.noreply.github.com>
2024-11-13 10:59:15 -0500

    create test route and handler

--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import (
 
 func main() {
 	app := fiber.New()
+	app.Get("/test", handleTest)
 	app.Listen(":5000")
+}
+
+func handleTest(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "I am working..."})
 }
